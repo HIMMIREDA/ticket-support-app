e904417349d81dbb69030809718081437276b58c
reda <reda.himmi@edu.uca.ma>
2022-09-20 00:03:27 +0100

    add serving frontend in production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const dotenv = require("dotenv").config();
 const express = require("express");
 const colors = require("colors");
@@ -12,13 +13,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the Support Desk API" });
-});
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes")); // users routes
 app.use("/api/tickets", require("./routes/ticketRoutes")); // tickets routes
+
+// SERVE Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the Support Desk API" });
+  });
+}
 
 app.use(errorHandler);
 
